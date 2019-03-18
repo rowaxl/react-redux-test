3e34b3e02af1bdb59b4b835a996b02506f3e7ed9
rowaxl <rowaxl0@gmail.com>
2019-03-19 08:16:32 +0900

    connect prototype components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import CommetnBox from './CommentBox';
+import CommentList from './CommentList';
 
 class App extends Component { 
     render() {
         return (
             <div>
-                app
+                <CommetnBox />
+                <CommentList />
             </div>
         );
     };
