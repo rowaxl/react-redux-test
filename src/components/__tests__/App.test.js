6c64bd109ccb981296c40298a60cf9c4d7871351
rowaxl <rowaxl0@gmail.com>
2019-03-20 21:00:01 +0900

    App component test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 
-it('some test', () => {
+describe('Check App component', () => {
     const wrapped = shallow(<App />);
 
-    expect(wrapped.find(CommentBox).length).toEqual(1);
+    it('Check App contains comment box', () => {
+        expect(wrapped.find(CommentBox).length).toEqual(1);
+    });
+    
+    it('Check App contains comment list', () => {
+        expect(wrapped.find(CommentList).length).toEqual(1);
+    });
 });
