a28fb84114eae507ad580bef4936532e49a65d45
rowaxl <rowaxl0@gmail.com>
2019-03-21 21:42:32 +0900

    basic textinput implements

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
-class CommentBox extends Component { 
+class CommentBox extends Component {
+    state = { comment: '' };
+
+    onCommentChange = event => {
+        this.setState({ comment: event.target.value });
+    }
+
+    onSubmitForm = event => {
+        event.preventDefault();
+
+        // call an action creator
+        // to save comment
+        this.setState({ comment: '' });
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.onSubmitForm}>
                 <h4>Add a comment</h4>
-                <textarea />
+                <textarea onChange={this.onCommentChange} value={this.state.comment} />
                 <div>
                     <button>Submit</button>
                 </div>
