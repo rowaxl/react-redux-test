1275b487c00302121e47d086c939386d1b7a9861
rowaxl <rowaxl0@gmail.com>
2019-03-21 21:36:19 +0900

    update comment box

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -3,9 +3,13 @@ import React, { Component } from 'react';
 class CommentBox extends Component { 
     render() {
         return (
-            <div>
-                CommentBox
-            </div>
+            <form>
+                <h4>Add a comment</h4>
+                <textarea />
+                <div>
+                    <button>Submit</button>
+                </div>
+            </form>
         );
     };
 }
