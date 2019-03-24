9f62ba1ab73fb4574968e3d76b51016a4f9654f7
rowaxl <rowaxl0@gmail.com>
2019-03-24 21:45:30 +0900

    add submit event test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -26,4 +26,13 @@ describe('test CommentBox components', () => {
         expect(wrapped.state('comment')).toEqual(value);
         expect(wrapped.find('textarea').prop('value')).toEqual(value);
     });
+
+    it('when click submit button, textarea will be clean', () => {
+        const value = 'new comment';
+        wrapped.find('textarea').simulate('change', { target: { value } });
+        wrapped.update();
+
+        wrapped.find('form').simulate('submit');
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    })
 });
