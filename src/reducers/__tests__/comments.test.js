d91c66d0995d74f44e75f01c7422894249ae9f9f
rowaxl <rowaxl0@gmail.com>
2019-03-28 20:48:33 +0900

    add reducer test

--- src/reducers/__tests__/comments.test.js
+++ src/reducers/__tests__/comments.test.js
@@ -1,1 +1,17 @@
 import commentReducer from 'reducers/comments';
+import { ADD_COMMENT } from 'actions/types';
+
+describe('test comments reducers', () => {
+    it('test add comment action', () => {
+        const newComment = 'New Comment';
+
+        const action = {
+            type: ADD_COMMENT,
+            payload: newComment
+        };
+    
+        const newState = commentReducer([], action);
+
+        expect(newState).toEqual([newComment]);
+    });
+});
--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,6 +1,8 @@
+import { ADD_COMMENT } from 'actions/types';
+
 export default (state = [], action) => {
     switch (action.type) {
-        case 'ADD_COMMENT':
+        case ADD_COMMENT:
             return [...state, action.payload];
         default:
             return state;
