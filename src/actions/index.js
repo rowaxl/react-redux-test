a06cb6550e44ee9825c464e8a84189b3a2203539
rowaxl <rowaxl0@gmail.com>
2019-03-29 21:35:32 +0900

    add fetch comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,15 @@
-import { ADD_COMMENT } from './types';
+import { ADD_COMMENT, FETCH_COMMENTS } from 'actions/types';
+import comments from 'api/comments';
 
 export const addComment = comment => {
     return { type: ADD_COMMENT, payload: comment };
+}
+
+export const fetchComments = async () => {
+    const response = await comments.get();
+
+    return {
+        type: FETCH_COMMENTS,
+        payload: response.data
+    };
 }
