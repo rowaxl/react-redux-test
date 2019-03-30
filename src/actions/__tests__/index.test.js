bb6e1fd3e734e7be3d9513a78b7dc1f436de2a62
rowaxl <rowaxl0@gmail.com>
2019-03-30 23:04:23 +0900

    no change

--- src/actions/__tests__/index.test.js
+++ src/actions/__tests__/index.test.js
@@ -4,14 +4,12 @@ import { ADD_COMMENT, FETCH_COMMENT } from 'actions/types';
 describe('addComment action test', () => {
     it('has the correct type', () => {
         const action = addComment();
-
         expect(action.type).toEqual(ADD_COMMENT);
     });
 
     it('has the correct payload', () => {
         const newComment = 'A new comment';
         const action = addComment(newComment);
-
         expect(action.payload).toEqual(newComment);
     });
 });
