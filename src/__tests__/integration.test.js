2e83907e1fbebecff1f43114181c07b5372fbe5d
rowaxl <rowaxl0@gmail.com>
2019-03-31 21:44:58 +0900

    add moxios

--- src/__tests__/integration.test.js
+++ src/__tests__/integration.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
 import Root from 'Root';
 import App from 'components/App';
 
 describe('integration test', () => {
+    beforeEach(() => {
+        moxios.install();
+        moxios.stubRequest('https://jsonplaceholder.typicode.com/comments', {
+            status: 200,
+            response: ["abcd"]
+        });
+    });
+
     it('can fetch a list of comments and display', () => {
         // render
         const wrapped = mount(
