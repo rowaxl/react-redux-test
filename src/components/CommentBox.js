8b5e51a08d0ddcf0f8e3f3c87f39709f11941b17
rowaxl <rowaxl0@gmail.com>
2019-04-08 14:09:18 +0900

    add history back function at comment box

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -5,6 +5,20 @@ import * as actions from 'actions';
 class CommentBox extends Component {
     state = { comment: '' };
 
+    componentDidMount = () => {
+        this.shouldNavigateAway();
+    }
+
+    componentDidUpdate = () => {
+        this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway = () => {
+        if (!this.props.auth) {
+            this.props.history.push('/');
+        }
+    }
+
     onCommentChange = event => {
         this.setState({ comment: event.target.value });
     }
@@ -31,4 +45,8 @@ class CommentBox extends Component {
     };
 }
 
-export default connect(null, actions)(CommentBox);
+const mapStateToProps = state => {
+    return { auth: state.auth };
+}
+
+export default connect(mapStateToProps, actions)(CommentBox);
