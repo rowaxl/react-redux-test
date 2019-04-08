7bce08ed03bc1d3fa0ca018feacbc75fc078c90b
rowaxl <rowaxl0@gmail.com>
2019-04-08 14:02:41 +0900

    add header and signin-out action

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import CommentBox from './CommentBox';
-import CommentList from './CommentList';
+import Header from 'components/Header';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 class App extends Component { 
     render() {
         return (
             <div>
+                <Header />
                 <Route path='/post' component={CommentBox}/>
                 <Route path='/' exact component={CommentList}/>
             </div>
--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,5 +1,8 @@
+import { CHANGE_AUTH } from 'actions/types';
 export default (state = false, action) => {
     switch (action.type) {
+        case CHANGE_AUTH:
+            return action.payload;
         default:
             return state;
     }
